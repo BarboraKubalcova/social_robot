139d894f0129c6cf550ca3f38cf6dcbfee826d79
Barbora Kubalcova <barbora.kubalcova@siemens-healthineers.com>
2025-02-27 17:05:44 +0100

    implemented slovak speech-to-text

---
+++ speech_recognition/main.py
@@ -0,0 +1,74 @@
+import speech_recognition as sr
+import pyttsx3 
+import sounddevice
+import pyaudio
+
+
+# Function to convert text to
+# speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+    
+    
+def get_speech(r, mic_id):
+        
+    try:
+        with sr.Microphone(device_index=mic_id) as source2:
+            print("Listening...")
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            
+            #listens for the user's input 
+            audio2 = r.listen(source2)
+            
+            # Using google to recognize audio
+            spoken_text = r.recognize_google(audio2, language="sk-SK")
+            spoken_text = spoken_text.lower()
+            print(f"Did you say: '{spoken_text}'?")
+            
+    except sr.RequestError as e:
+        print(f"Could not request results: '{str(e)}'")
+    except sr.UnknownValueError:
+        print(f"unknown error occurred")
+
+    return spoken_text
+
+
+def get_mic_id():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    mic_id = None
+
+    for i in range(0, numdevices):
+            device_name = p.get_device_info_by_host_api_device_index(0, i).get('name')
+            max_input_channel = p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')
+            # if max_input_channel > 0:
+            #     print(f"Input Device id {i} - {device_name}")
+            if "ReSpeaker" in device_name and max_input_channel > 0:
+                    mic_id = i
+    if not mic_id:
+        raise Exception("No Respeaker microphone detected.")
+     
+    return mic_id
+
+
+
+def main():
+    r = sr.Recognizer() 
+    
+    mic_id = get_mic_id() # also checks if mic is connected
+    print(f"ReSpeaker microphone id is: {mic_id}")
+    text = get_speech(r, mic_id) 
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+https://wiki.seeedstudio.com/ReSpeaker_Mic_Array_v2.0/#extract-voice
+
+"""
